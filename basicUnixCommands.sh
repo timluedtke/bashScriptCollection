aa7a1083be0758d9d5da8f3a0f6e2791bc871e5f
Tim L:udtke <timluedtke@users.noreply.github.com>
2019-05-28 10:30:18 +0200

    Create basicUnixCommands.sh

---
+++ basicUnixCommands.sh
@@ -0,0 +1,2 @@
+inxi -Fxz   # This command shows system specs in great detail
+cat /etc/*release # Show details about linux distribution in use (e.g. which version of ubuntu), if uname is not enough
