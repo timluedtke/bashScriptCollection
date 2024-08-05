a35a0b948ed0afab7b330e0380ef7b773893d0a0
Tim L:udtke <timluedtke@users.noreply.github.com>
2024-08-05 12:53:34 +0200

    Create addCustomBackgroundToMsTeamsMacOs.sh

---
+++ addCustomBackgroundToMsTeamsMacOs.sh
@@ -0,0 +1,20 @@
+# Thanks to reddit user: /u/myers022/
+
+# Check if the output directory exists; if not, create it
+[[ ! -d "$outputPath" ]] && mkdir -p "$outputPath"
+
+# Loop through all PNG images in the specified directory
+for image in $imagePath/*.png; do
+
+# Generate a new GUID
+guid=$(uuidgen)
+
+# Resize main image to 1920x1080
+echo "Creating Background"
+sips -z 1080 1920 "$image" --out "$outputPath/$guid.png"
+
+# Resize thumbnail to 220x158
+echo "Creating Background Thumbnail"
+thumbName="${guid}_thumb.png"
+sips -z 158 220 "$image" --out "$outputPath/$thumbName"
+done
